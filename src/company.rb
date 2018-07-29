9f3031cc6a076c17bcbef8e72f11c3f0d9bc5e0b
Alex-Diez <alex.dukhno@icloud.com>
2018-07-29 19:28:37 +0300

    two employees in a list test

--- src/company.rb
+++ src/company.rb
@@ -1,13 +1,20 @@
 class Company
   def emails_by_highest_position(employees, limit)
-    [employees.first.email]
+    employee = employees[0]
+    if employees.size > 1
+      if employees[1].position > employee.position
+        return [employees[1].email]
+      end
+    end
+    [employee.email]
   end
 end
 
 class Employee
-  attr_reader :email
+  attr_reader :email, :position
 
   def initialize(position, email)
     @email = email
+    @position = position
   end
 end
--- test/company_test.rb
+++ test/company_test.rb
@@ -2,10 +2,29 @@ require 'rspec'
 require 'company'
 
 describe 'Company' do
+  before(:each) do
+    @company = Company.new
+  end
+
   it 'extracts employee email from single employee list' do
-    company = Company.new
-    employees = [Employee.new(:JUNIOR, "junior@company.com")]
-    emails = company.emails_by_highest_position(employees, 1)
+    employees = [junior]
+    emails = @company.emails_by_highest_position(employees, 1)
     expect(emails).to eq(["junior@company.com"])
   end
+
+  it 'extracts employee email with highest position from list of two employees' do
+    employees = [junior, middle]
+    emails = @company.emails_by_highest_position(employees, 1)
+    expect(emails).to eq(["middle@company.com"])
+  end
+
+  private
+
+    def junior
+      Employee.new(:JUNIOR, "junior@company.com")
+    end
+
+    def middle
+      Employee.new(:MIDDLE, "middle@company.com")
+    end
 end
